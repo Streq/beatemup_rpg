429e9f81eb5077159ce60c5d18db359042c2a2fd
streq <strequena@gmail.com>
2023-04-21 21:48:14 -0300

    some punching knockback

---
+++ src/fighter/hitbox/hitbox.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+export var active := true setget set_active
+func set_active(val):
+	active = val
+	monitorable = active
+	monitoring = active
+
+
+func _on_area_entered(hurtbox):
+	if owner == hurtbox.owner:
+		return
+	hurtbox.owner.velocity = Vector2(owner.facing_dir*100.0, -50.0)
+func _ready() -> void:
+	connect("area_entered",self,"_on_area_entered")

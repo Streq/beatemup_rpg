a053bd7f7cad8e1206dc826a842ab4822ee92ad0
streq <strequena@gmail.com>
2023-05-30 10:10:38 -0300

    primer nivel copiado de kirby

--- src/global/palette_filter/bad_shader_rounding_workaround.gd
+++ src/global/palette_filter/bad_shader_rounding_workaround.gd
@@ -3,6 +3,6 @@ tool
 
 func _ready() -> void:
 	for i in 4:
-		get_parent().palette[i].b = i/255.0
+		get_parent().palette[i].b = round(get_parent().palette[i].b*255.0)/255.0
 	if !Engine.editor_hint:
 		queue_free()

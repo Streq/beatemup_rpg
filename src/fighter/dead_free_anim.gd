49f354952c9496107661ddd9914927b0c8082a3e
streq <strequena@gmail.com>
2023-06-11 10:31:19 -0300

    death particles

--- src/fighter/dead_free_anim.gd
+++ src/fighter/dead_free_anim.gd
@@ -1,14 +1,17 @@
 extends Node
 var played = false
+onready var death_particles: CPUParticles2D = $"%death_particles"
+
 func play():
 	if played:
 		return
 	played = true
 	get_node("%hurtbox").queue_free()
 	get_node("%hitbox").queue_free()
-	var tween = create_tween()
-	tween.tween_property(owner,"visible",false, 0.1)
-	tween.tween_property(owner,"visible",true, 0.1)
-	tween.set_loops(5)
-	yield(tween,"finished")
+#	var tween = create_tween()
+#	tween.tween_property(owner,"visible",false, 0.1)
+#	tween.tween_property(owner,"visible",true, 0.1)
+#	tween.set_loops(5)
+#	yield(tween,"finished")
+	death_particles.trigger()
 	owner.despawn()

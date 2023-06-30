96c67bbe1784d7c946480e2b86ee0c1ddd0a0115
streq <strequena@gmail.com>
2023-06-30 10:00:54 -0300

    anda perfecto falta poder ubicarlo arriba o abajo al TEXTBOX, y que si agrego varios mensajes aparezca la flechita

--- src/global/TextBox/TextBox.gd
+++ src/global/TextBox/TextBox.gd
@@ -1,61 +1,97 @@
 extends CanvasLayer
+signal next
 
 onready var lines_container: VBoxContainer = $"%lines_container"
 
 onready var arrow: TextureRect = $"%arrow"
 
-signal next
+onready var pause_client: Node = $"%pause_client"
 
 
 func _ready():
-	yield(wait_for_input(), "completed")
-	display_text(
-		"""Hola amigos de youtube yo soy santi requena pero pueden decirme santicapo. 
+	hide()
+	yield(_wait_for_input(), "completed")
+	text(
+		"""Hola, amigos de youtube! yo soy santi requena pero pueden decirme santicapo. 
 		Y vengo a enseñarles como instalar el sims 4 con crack.
-		Lo primero que tienen que hacer es ser muy buenos jugando al sims hermano. 
-		Es asi, que queres que te diga.""" 
+		Lo primero que tienen que hacer es descargar el sims 4 de taringa.
+		Yo no lo descargo porque ya lo tengo."""
 	)
+	text("que puto que so")
+	text("hola chupaverga")
+	text("PIBE: cuchame fresco")
+	text("BETO: si que queres")
+	text("PIBE: quiero a tu hermana boludo")
+	text("PIBE: cucha wachin")
+	text("PIBE: quiero flashear ser pabre")
 
 
-func display_text(text: String):
+var text_queue = []
+
+
+func _process(delta: float) -> void:
+	if displaying:
+		return
+	if text_queue.empty():
+		pause_client.unpause()
+		return
+	_display_text(text_queue.pop_front())
+
+
+func text(text: String):
+	text = text.strip_edges()
+	if !text:
+		return
+	text_queue.append(text)
+
+
+var displaying := false
+
+
+func _display_text(text: String):
+	pause_client.pause()
+	show()
+	displaying = true
 	var texts = text.split("\n", false)
 
 	var line_labels = lines_container.get_children()
 	var current_paragraph = -1
 	for paragraph in texts:
 		current_paragraph += 1
-		set_word_source(paragraph)
-		
+		_set_word_source(paragraph)
+
 		line_index = 0
 		shown_lines = 0
-		hide_arrow()
-		reset_labels_text()
-		
-		if !has_next_word():
+		_hide_arrow()
+		_reset_labels_text()
+
+		yield(get_tree().create_timer(0.1), "timeout")
+		if !_has_next_word():
 			continue
 
-		line_labels[0].text = get_next_word()
+		line_labels[0].text = _get_next_word()
 
-		while has_next_word():
-			if next_word_overflows_line():
+		while _has_next_word():
+			if _next_word_overflows_line():
 				line_index += 1
 				if line_index >= max_lines:
-					yield(show_characters_one_by_one(), "completed")
+					yield(_show_characters_one_by_one(), "completed")
 
-					show_arrow()
-					yield(wait_for_input(), "completed")
+					_show_arrow()
+					yield(_wait_for_input(), "completed")
 
-					hide_arrow()
-					yield(scroll_one_line(), "completed")
-			
-#			yield(wait_for_input(), "completed")
-			add_word(line_labels[line_index], get_next_word())
+					_hide_arrow()
+					yield(_scroll_one_line(), "completed")
 
-		yield(show_characters_one_by_one(), "completed")
-		if current_paragraph < texts.size()-1:
-			show_arrow()
-		yield(wait_for_input(), "completed")
-		hide_arrow()
+#			yield(_wait_for_input(), "completed")
+			_add_word(line_labels[line_index], _get_next_word())
+
+		yield(_show_characters_one_by_one(), "completed")
+		if current_paragraph < texts.size() - 1:
+			_show_arrow()
+		yield(_wait_for_input(), "completed")
+	hide()
+	displaying = false
 
 
 var words: PoolStringArray
@@ -63,11 +99,11 @@ var line_index := 0
 onready var max_lines := lines_container.get_child_count()
 
 
-func set_word_source(paragraph: String):
+func _set_word_source(paragraph: String):
 	words = paragraph.split(" ", false)
 
 
-func reset_labels_text():
+func _reset_labels_text():
 	for label in lines_container.get_children():
 		label.text = ""
 
@@ -75,7 +111,7 @@ func reset_labels_text():
 onready var animation_player: AnimationPlayer = $"%AnimationPlayer"
 
 
-func scroll_one_line():
+func _scroll_one_line():
 	animation_player.play("scroll_one_line")
 	line_index -= 1
 	shown_lines -= 1
@@ -83,49 +119,64 @@ func scroll_one_line():
 	pass
 
 
-func new_line():
+func _new_line():
 	var lines = lines_container.get_children()
 	for i in lines.size() - 1:
 		lines[i].text = lines[i + 1].text
 	lines[-1].text = ""
 
 
-func has_next_word():
+func _has_next_word():
 	return words.size() > 0
 
 
-func next_word_overflows_line() -> bool:
+func _next_word_overflows_line() -> bool:
 	var lines = lines_container.get_children()
 	var line: Label = lines[line_index]
 	var aux = line.text
-	add_word(line, words[0])
+	_add_word(line, words[0])
 	var ret = line.get_line_count() > 1
 	line.text = aux
 	return ret
 
-static func add_word(label: Label, word: String) -> void:
+
+static func _add_word(label: Label, word: String) -> void:
 	if label.text:
 		label.text += " " + word
 	else:
 		label.text = word
 
-func get_next_word() -> String:
+
+func _get_next_word() -> String:
 	var ret = words[0]
 	words.remove(0)
 	return ret
 
 
-func wait_for_input():
+func _wait_for_input():
 	yield(self, "next")
+
 
 var shown_lines := 0
 var characters_shown_since_last_frame := 0
-func show_characters_one_by_one():
+var fast_chars_per_frame = 5
+var normal_chars_per_frame = 1
+var chars_per_frame = 0
+
+
+func _refresh_chars_per_frame():
+	chars_per_frame = (
+		fast_chars_per_frame
+		if Input.is_action_pressed("B") or Input.is_action_pressed("A")
+		else normal_chars_per_frame
+	)
+
+
+func _show_characters_one_by_one():
 	var aux = []
-	
+
 	var lines = lines_container.get_children()
 	aux.resize(lines.size())
-	
 	for i in range(shown_lines, lines.size()):
 		var line = lines[i]
 		aux[i] = line.text
@@ -133,21 +184,28 @@ func show_characters_one_by_one():
 	for i in range(shown_lines, lines.size()):
 		var text = aux[i]
 		var line = lines[i]
+		_refresh_chars_per_frame()
 		for c in text:
 			line.text += c
 			characters_shown_since_last_frame += 1
-			if characters_shown_since_last_frame >= 3:
+			if characters_shown_since_last_frame >= chars_per_frame:
 				characters_shown_since_last_frame = 0
 				yield(get_tree().create_timer(0.05), "timeout")
+				_refresh_chars_per_frame()
 		shown_lines += 1
 
+
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("A"):
+	if event.is_action_pressed("A") or event.is_action_pressed("B"):
 		emit_signal("next")
-		
+
 
 onready var arrow_animation: AnimationPlayer = $"%arrow_animation"
-func show_arrow():
+
+
+func _show_arrow():
 	arrow_animation.play("blink")
-func hide_arrow():
+
+
+func _hide_arrow():
 	arrow_animation.play("hide")
